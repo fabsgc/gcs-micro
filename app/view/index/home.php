1a1dfcc90421a1aa3681790a49a381e192643055
fabsgc <fabienbeaudimi@hotmail.fr>
2016-10-12 17:16:51 +0200

    micro fix with some bugs and unexpected files

--- app/view/index/home.php
+++ app/view/index/home.php
@@ -1,9 +1,10 @@
-<!Doctype html>
+<!doctype html>
+<html lang="fr">
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 	<title>GCsystem Micro</title>
 	<link rel="stylesheet" type="text/css" href="/<?php echo FOLDER; ?>asset/css/default.css">
-	<link rel="icon" type="image/png" href="/<?php echo FOLDER; ?>asset/images/logo.png" />
+	<link rel="icon" type="image/png" href="/<?php echo FOLDER; ?>asset/img/logo.png" />
 </head>
 <body>
 	<h1 class="standalone-title">GCsystem Micro</h1>
